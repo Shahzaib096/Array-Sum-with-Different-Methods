a83a5cf88c9aa919088a66ad3d4c78803019545d
Shahzaib096 <61833621+Shahzaib096@users.noreply.github.com>
2020-03-05 18:41:18 +0500

    Add files via upload

---
+++ Array-Sum-with-Different-Methods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sumArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using Sum Method Array Sum()
+            int[] array = { 1, 2, 3, 5, 56, 54 };
+            int sum = array.Sum();
+            Console.WriteLine("Sum of Array :" + sum);
+            Console.ReadLine();
+
+            //Using Array.For EachMethod()
+            int ArraySum = 0;
+            Array.ForEach(array,i=> ArraySum+=i);
+            Console.WriteLine("Array Sum is :"+ArraySum);
+
+            Console.ReadLine();
+
+            //Print array Values
+            foreach(int item in array)
+            {
+                Console.WriteLine("Array Vlaues: "+item);
+            }
+
+            //Using ForEach Loop
+            int sumOfArray = 0;
+            foreach(int item in array)
+            {
+                sumOfArray += item;
+            }
+            Console.WriteLine("Array Sum is : "+sumOfArray);
+            Console.ReadLine();
+
+
+           
+
+        }
+      
+       
+    }
+}
